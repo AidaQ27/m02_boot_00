ab03f61eb40b1f13bd15869a00042c7ba13d9fc5
Aida Queralt <aidaqueraltt@gmail.com>
2022-01-17 16:01:19 +0100

    ejercicio perro

--- perro.py
+++ perro.py
@@ -1,5 +1,16 @@
 class Perro():
-    def _init_(self, nommbre, edad, peso):
-        self.nombre = nombre
-        self.edad = edad
-        self.peso = peso
+    def __init__(self, n, e, p):
+        self.nombre = n
+        self.edad = e
+        self.peso = p
+    
+    def ladrar(self):
+        if self.peso >= 8:
+            print('Guau, guau')
+        else:
+            print('boop, boop')
+    
+    def __str__(self):
+        return"Soy el perro {}".format(self.nombre)
+        return"Perro {}, e: {}, p: {}".format(self.nombre, self.edad, self.peso)
+
